d64d21d02d38749eb58acb8f03389ba724313efe
Kongnakorn Jantakun <kongnakornjantakun@gmail.com>
2026-07-07 19:42:11 +0700

    no message

--- src/app/shared/i18n/presentation/pages/language-selector/language-selector.component.ts
+++ src/app/shared/i18n/presentation/pages/language-selector/language-selector.component.ts
@@ -5,14 +5,15 @@ import { I18nService } from '../../../data/i18n.service';
   selector: 'app-language-selector',
   standalone: true,
   template: `
-    <div class="text-center mb-2">
-      <a href="javascript:void(0)" (click)="i18n.loadLanguage('en')">
-        <img src="assets/lang/en.svg" height="25" title="English">
-      </a>
-      <a href="javascript:void(0)" (click)="i18n.loadLanguage('th')">
-        <img src="assets/lang/th.svg" height="25" title="ไทย">
-      </a>
+    <div class="language-switcher">
+          <a href="javascript:void(0)" (click)="i18n.loadLanguage('en')">
+            <img src="assets/lang/en.svg" height="25" title="English">
+          </a>
+          <a href="javascript:void(0)" (click)="i18n.loadLanguage('th')">
+            <img src="assets/lang/th.svg" height="25" title="ไทย">
+          </a>
     </div>
+    <br>
   `,
 })
 export class LanguageSelectorComponent {
