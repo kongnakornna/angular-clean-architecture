fa16d0e92d61c774dbedf345b06b084ce05682d7
Kongnakorn Jantakun <kongnakornjantakun@gmail.com>
2026-08-16 20:28:48 +0700

    displayYear

--- src/app/layouts/footer/footer.component.spec.ts
+++ src/app/layouts/footer/footer.component.spec.ts
@@ -4,6 +4,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { FooterComponent } from './footer.component';
 import { TranslatePipe } from '../../shared/pipes/translate.pipe';
+import { I18nService } from '../../shared/i18n/data/i18n.service';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
@@ -30,5 +31,16 @@ describe('FooterComponent', () => {
 
   it('should have current year', () => {
     expect(component.currentYear).toBe(new Date().getFullYear());
+  });
+
+  it('should show Buddhist year when language is th', () => {
+    TestBed.inject(I18nService).lang.set('th');
+    fixture.detectChanges();
+    expect(component.displayYear()).toBe(new Date().getFullYear() + 543);
+  });
+
+  it('should show current year when language is not th', () => {
+    fixture.detectChanges();
+    expect(component.displayYear()).toBe(new Date().getFullYear());
   });
 });
