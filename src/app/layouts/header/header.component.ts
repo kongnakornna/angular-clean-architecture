cb28d245525d66ffe02e7b11f27af042a7e15b4d
Kongnakorn Jantakun <kongnakornjantakun@gmail.com>
2026-08-18 08:57:15 +0700

    no message

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -21,40 +21,51 @@ interface MenuItem {
 export class HeaderComponent {
   private layout = inject(LayoutService);
   private sanitizer = inject(DomSanitizer);
-
-  private invoiceIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-book"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /><path d="M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0" /><path d="M3 6l0 13" /><path d="M12 6l0 13" /><path d="M21 6l0 13" /></svg>`;
+  private expIcon = ``;
+  private invoiceIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-file-invoice">
+	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	<path d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005zm4 15h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0 -2m0 -4h-8a1 1 0 0 0 0 2h8a1 1 0 0 0 0 -2m-7 -7h-1a1 1 0 1 0 0 2h1a1 1 0 1 0 0 -2" />
+	<path d="M19 7h-4l-.001 -4.001z" />
+</svg>`;
   private themeIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments-pause">
-	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	<path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-	<path d="M6 4v4" />
-	<path d="M6 12v8" />
-	<path d="M13.627 14.836a2 2 0 1 0 -.62 2.892" />
-	<path d="M12 4v10" />
-	<path d="M12 18v2" />
-	<path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
-	<path d="M18 4v1" />
-	<path d="M17 17v5" />
-	<path d="M21 17v5" />
-	<path d="M18 9v4.5" />
-</svg>`;
+    	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    	<path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+    	<path d="M6 4v4" />
+    	<path d="M6 12v8" />
+    	<path d="M13.627 14.836a2 2 0 1 0 -.62 2.892" />
+    	<path d="M12 4v10" />
+    	<path d="M12 18v2" />
+    	<path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+    	<path d="M18 4v1" />
+    	<path d="M17 17v5" />
+    	<path d="M21 17v5" />
+    	<path d="M18 9v4.5" />
+    </svg>`;
   private languageIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-language">
-	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	<path d="M9 6.371c0 4.418 -2.239 6.629 -5 6.629" />
-	<path d="M4 6.371h7" />
-	<path d="M5 9c0 2.144 2.252 3.908 6 4" />
-	<path d="M12 20l4 -9l4 9" />
-	<path d="M19.1 18h-6.2" />
-	<path d="M6.694 3l.793 .582" />
-</svg>`;
-  private AnalyticsIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-chart-histogram">
-	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
-	<path d="M3 3v18h18" />
-	<path d="M20 18v3" />
-	<path d="M16 16v5" />
-	<path d="M12 13v8" />
-	<path d="M8 16v5" />
-	<path d="M3 11c6 0 5 -5 9 -5s3 5 9 5" />
-</svg>`;
+    	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    	<path d="M9 6.371c0 4.418 -2.239 6.629 -5 6.629" />
+    	<path d="M4 6.371h7" />
+    	<path d="M5 9c0 2.144 2.252 3.908 6 4" />
+    	<path d="M12 20l4 -9l4 9" />
+    	<path d="M19.1 18h-6.2" />
+    	<path d="M6.694 3l.793 .582" />
+    </svg>`;
+  private AnalyticsIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-chart-area">
+    	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    	<path d="M20 18a1 1 0 0 1 .117 1.993l-.117 .007h-16a1 1 0 0 1 -.117 -1.993l.117 -.007h16z" />
+    	<path d="M15.22 5.375a1 1 0 0 1 1.393 -.165l.094 .083l4 4a1 1 0 0 1 .284 .576l.009 .131v5a1 1 0 0 1 -.883 .993l-.117 .007h-16.022l-.11 -.009l-.11 -.02l-.107 -.034l-.105 -.046l-.1 -.059l-.094 -.07l-.06 -.055l-.072 -.082l-.064 -.089l-.054 -.096l-.016 -.035l-.04 -.103l-.027 -.106l-.015 -.108l-.004 -.11l.009 -.11l.019 -.105c.01 -.04 .022 -.077 .035 -.112l.046 -.105l.059 -.1l4 -6a1 1 0 0 1 1.165 -.39l.114 .05l3.277 1.638l3.495 -4.369z" />
+    </svg>`;
+  private invoicesIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-file-invoice">
+        	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        	<path d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005zm4 15h-2a1 1 0 0 0 0 2h2a1 1 0 0 0 0 -2m0 -4h-8a1 1 0 0 0 0 2h8a1 1 0 0 0 0 -2m-7 -7h-1a1 1 0 1 0 0 2h1a1 1 0 1 0 0 -2" />
+        	<path d="M19 7h-4l-.001 -4.001z" />
+        </svg>`;
+  private batchIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" class="icon icon-tabler icons-tabler-filled icon-tabler-file-time">
+    	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    	<path d="M12 2l.117 .007a1 1 0 0 1 .876 .876l.007 .117v4l.005 .15a2 2 0 0 0 1.838 1.844l.157 .006h4l.117 .007a1 1 0 0 1 .876 .876l.007 .117v9a3 3 0 0 1 -2.824 2.995l-.176 .005h-10a3 3 0 0 1 -2.995 -2.824l-.005 -.176v-14a3 3 0 0 1 2.824 -2.995l.176 -.005zm0 8a5 5 0 0 0 -4.995 4.783l-.005 .217a5 5 0 1 0 5 -5m0 2a3 3 0 1 1 0 6a3 3 0 0 1 0 -6m0 .496a1 1 0 0 0 -1 1v1.504a1 1 0 0 0 .293 .707l1 1a1 1 0 0 0 1.414 0l.083 -.094a1 1 0 0 0 -.083 -1.32l-.707 -.708v-1.089a1 1 0 0 0 -1 -1" />
+    	<path d="M19 7h-4l-.001 -4.001z" />
+    </svg>`;
+
   unreadCount = 3;
 
   trustHtml(html: string): SafeHtml {
@@ -108,7 +119,7 @@ export class HeaderComponent {
     { label: "Documents", icon: "folder", route: "/documents" },
     { label: "Job Cards", icon: this.invoiceIcon, route: "/jobs" },
     { label: "Work Orders", icon: "clipboard", route: "/wos/orders" },
-    { label: "Batch Jobs", icon: this.invoiceIcon, route: "/batch/jobs" },
+    { label: "Batch Jobs", icon: this.batchIcon, route: "/batch/jobs" },
     { label: "IoT Devices", icon: this.invoiceIcon, route: "/iot/devices" },
     { label: "Analytics", icon: this.invoiceIcon, route: "/analytics" },
     { label: "Reports", icon: this.invoiceIcon, route: "/reports" },
@@ -136,7 +147,7 @@ export class HeaderComponent {
       route: "/payments",
       children: [
         { label: "Payments", icon: "credit-card", route: "/payments" },
-        { label: "Invoices", icon: this.invoiceIcon, route: "/invoices" },
+        { label: "Invoices", icon: this.invoicesIcon, route: "/invoices" },
         { label: "Job Cards", icon: this.invoiceIcon, route: "/jobs" },
         { label: "Work Orders", icon: "clipboard", route: "/wos/orders" },
         { label: "Batch Jobs", icon: this.invoiceIcon, route: "/batch/jobs" },
